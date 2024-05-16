7751914a60b0fbdac8a5cbcc88dfac51ab9b5dc8
Elias Ribeiro <eliasjunio.riibeiro95@gmail.com>
2024-05-16 17:07:39 -0300

    update

--- script.js
+++ script.js
@@ -5,11 +5,19 @@
     if (window.innerWidth > minWidthForChatbot) {
         // Inicializa o Blip Chat apenas se a largura da tela for maior que o limite
         (function () {
-            new BlipChat()
-            .withAppKey('cG9ydGlmb2xpbzg6NjRlYmI3NTYtMTc5Yy00MmIwLWFmZjQtYzM4NGQ3NTFkNzNm')
+            window.onload = function () {
+                var blipClient = new BlipChat();
+                blipClient.withAppKey('cG9ydGlmb2xpbzg6NjRlYmI3NTYtMTc5Yy00MmIwLWFmZjQtYzM4NGQ3NTFkNzNm')
+            .withEventHandler(BlipChat.LOAD_EVENT, function () {
+          blipClient.sendMessage({
+                    "type": "text/plain",
+                    "content": "This is my first message"
+            });
+                })
             .withButton({"color":"#333","icon":"https://blipmediastore.blip.ai/public-medias/Media_a4a37081-2224-47b6-94cf-5aff5fa9cfd2"})
-            .withCustomCommonUrl('https://elias-junio-bqqie.chat.blip.ai/')
-            .build();
+                    .withCustomCommonUrl('https://elias-junio-bqqie.chat.blip.ai/')
+                .build();
+            }
         })();
     }
 
