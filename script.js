b4f68989c7876119832229d7e9c30ea173b15f21
Elias Ribeiro <eliasjunio.riibeiro95@gmail.com>
2024-05-16 17:24:14 -0300

    update

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
- // Define o limite de largura da tela para inicializar o Blip Chat
-    var minWidthForChatbot = 768;
+// Define o limite de largura da tela para inicializar o Blip Chat
+var minWidthForChatbot = 768;
 
-    // Verifica se a largura da janela é maior que o limite definido
-    if (window.innerWidth > minWidthForChatbot) {
-        
-        // Inicializa o Blip Chat apenas se a largura da tela for maior que o limite
-        (function () {
-            window.onload = function () {
-               // Função para vibrar a tela
-        function vibrateScreen() {
-            if (navigator.vibrate) {
-                navigator.vibrate(200);
+// Verifica se a largura da janela é maior que o limite definido
+if (window.innerWidth > minWidthForChatbot) {
+    (function () {
+        window.onload = function () {
+            // Função para vibrar a tela
+            function vibrateScreen() {
+                if (navigator.vibrate) {
+                    navigator.vibrate(200);
+                }
             }
-        }
-        // Configurar a vibração a cada 5 segundos (5000 milissegundos)
-        setInterval(vibrateScreen, 5000);
+            
+            // Configurar a vibração a cada 5 segundos (5000 milissegundos)
+            setInterval(vibrateScreen, 5000);
 
-                var blipClient = new BlipChat();
-                blipClient.withAppKey('cG9ydGlmb2xpbzg6NjRlYmI3NTYtMTc5Yy00MmIwLWFmZjQtYzM4NGQ3NTFkNzNm')
+            var blipClient = new BlipChat();
+            blipClient.withAppKey('cG9ydGlmb2xpb2xpbzg6NjRlYmI3NTYtMTc5Yy00MmIwLWFmZjQtYzM4NGQ3NTFkNzNm')
             .withEventHandler(BlipChat.LOAD_EVENT, function () {
-          blipClient.sendMessage({
+                blipClient.sendMessage({
                     "type": "text/plain",
                     "content": "Olá"
-            });
-                })
+                });
+            })
             .withButton({"color":"#333","icon":"https://blipmediastore.blip.ai/public-medias/Media_a4a37081-2224-47b6-94cf-5aff5fa9cfd2"})
-                    .withCustomCommonUrl('https://elias-junio-bqqie.chat.blip.ai/')
-                .build();
-            }
-        })();
-    }
+            .withCustomCommonUrl('https://elias-junio-bqqie.chat.blip.ai/')
+            .build();
+        }
+    })();
+}
+
 
     function updateProgress(barId, textId, percentage) {
         var bar = document.getElementById(barId);
