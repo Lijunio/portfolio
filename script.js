f532c80a70cdb611f8cd8ac1d2bfd205f79604a1
Elias Ribeiro <eliasjunio.riibeiro95@gmail.com>
2024-06-06 12:22:14 -0300

    update js

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var minWidthForChatbot = 768;
+// Define o limite de largura da tela para inicializar o Blip Chat
+var minWidthForChatbot = 768;
 
-    if (window.innerWidth > minWidthForChatbot) {
-        var blipClient = new BlipChat();
-        blipClient.withAppKey('cG9ydGlmb2xpbzg6NjRlYmI3NTYtMTc5Yy00MmIwLWFmZjQtYzM4NGQ3NTFkNzNm')
-            .withEventHandler(BlipChat.LOAD_EVENT, function () {
-                blipClient.sendMessage({
-                    "type": "text/plain",
-                    "content": "Olá"
-                });
-            })
-            .withButton({"color":"#333","icon":"https://blipmediastore.blip.ai/public-medias/Media_a4a37081-2224-47b6-94cf-5aff5fa9cfd2"})
-            .withCustomCommonUrl('https://elias-junio-bqqie.chat.blip.ai/')
-            .build();
+// Verifica se a largura da janela é maior que o limite definido
+if (window.innerWidth > minWidthForChatbot) {
+    (function () {
+        window.onload = function () {
+            var blipClient = new BlipChat();
+            blipClient.withAppKey('cG9ydGlmb2xpbzg6NjRlYmI3NTYtMTc5Yy00MmIwLWFmZjQtYzM4NGQ3NTFkNzNm')
+                .withEventHandler(BlipChat.LOAD_EVENT, function () {
+                    blipClient.sendMessage({
+                        "type": "text/plain",
+                        "content": "Olá"
+                    });
+                })
+                .withButton({"color":"#333","icon":"https://blipmediastore.blip.ai/public-medias/Media_a4a37081-2224-47b6-94cf-5aff5fa9cfd2"})
+                .withCustomCommonUrl('https://elias-junio-bqqie.chat.blip.ai/')
+                .build();
 
-        var chatButton = document.getElementById('blip-chat-open-iframe');
-        chatButton.style.display = 'block';
+            var chatButton = document.getElementById('blip-chat-open-iframe');
+            chatButton.style.display = 'block'; 
 
-        setTimeout(function () {
-            chatButton.classList.add('blip-chat-visible');
-        }, 5000);
+            setTimeout(function () {
+                chatButton.classList.add('blip-chat-visible');
+            }, 5000); 
 
-        chatButton.addEventListener('click', function () {
-            chatButton.classList.add('blip-chat-clicked');
-        });
+            chatButton.addEventListener('click', function () {
+                chatButton.classList.add('blip-chat-clicked');
+            });
+
+            // Verifica se os textos ultrapassam 13 linhas e exibe o botão "Continuar lendo" se necessário
+            var cardTexts = document.querySelectorAll('.card-text');
+            cardTexts.forEach(function (text) {
+                var lineHeight = parseInt(window.getComputedStyle(text).lineHeight);
+                var maxLines = 15;
+                if (text.scrollHeight > lineHeight * maxLines) {
+                    text.nextElementSibling.style.display = 'block'; // Exibe o botão "Continuar lendo"
+                }
+            });
+        };
+    })();
+}
+
+
+function updateProgress(barId, textId, percentage) {
+    var bar = document.getElementById(barId);
+    var text = document.getElementById(textId);
+    bar.style.width = percentage + '%';
+    if (percentage >= 100) {
+        bar.parentElement.classList.add('completed');
+        text.textContent = '100% Concluído';
+    } else {
+        text.textContent = percentage + '% Concluído';
     }
-
-    function updateProgress(barId, textId, percentage) {
-        var bar = document.getElementById(barId);
-        var text = document.getElementById(textId);
-        bar.style.width = percentage + '%';
-        if (percentage >= 100) {
-            bar.parentElement.classList.add('completed');
-            text.textContent = '100% Concluído';
-        } else {
-            text.textContent = percentage + '% Concluído';
-        }
-    }
+}
 
     // Pequenas telas
     updateProgress('progress-1', 'progress-text-1', 100);
@@ -48,16 +64,6 @@ document.addEventListener('DOMContentLoaded', function () {
     updateProgress('progress-2-large', 'progress-text-2-large', 100);
     updateProgress('progress-3-large', 'progress-text-3-large', 40);
 
-
-     // Adiciona o botão "Continuar lendo" apenas se necessário
-     var cardTexts = document.querySelectorAll('.card-text');
-     cardTexts.forEach(function (text) {
-         var lineHeight = parseInt(window.getComputedStyle(text).lineHeight);
-         var maxLines = 15;
-         if (text.scrollHeight > lineHeight * maxLines) {
-             text.nextElementSibling.style.display = 'block'; // Exibe o botão "Continuar lendo"
-         }
-     });
  
      window.toggleText = function (button) {
          const cardText = button.previousElementSibling;
@@ -69,4 +75,3 @@ document.addEventListener('DOMContentLoaded', function () {
              button.textContent = 'Mostrar menos';
          }
      };
- });
